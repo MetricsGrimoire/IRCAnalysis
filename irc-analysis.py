cc3a2a94edfe61f90c43a852555d0a287748e030
Alvaro del Castillo <acs@bitergia.com>
2013-08-05 13:38:15 +0200

    License changed to GPLv3

--- irc-analysis.py
+++ irc-analysis.py
@@ -4,24 +4,28 @@
 # This script parse IRC logs from Wikimedia 
 # and store them in database
 
-# Copyright (C) 2013 Bitergia
-
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-
+# 
+# Copyright (C) 2012-2013 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
-
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-# 02110-1301, USA.
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #
-# Author: Alvaro del Castillo <acs@bitergia.com>
+# This file is a part of the VizGrimoireJS package
+#
+# Authors:
+#   Alvaro del Castillo San Felix <acs@bitergia.com>
+#
 
 from optparse import OptionParser
 import os, sys
