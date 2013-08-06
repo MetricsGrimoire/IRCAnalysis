75dba77386a4ca3698385e8866b1184271609004
Alvaro del Castillo <acs@bitergia.com>
2013-08-06 04:48:19 +0200

    Changed all to GPLv3 and use the same format

--- irc_analysis.py
+++ irc_analysis.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+#
 # This script parse IRC logs from Wikimedia 
 # and store them in database
-
 # 
 # Copyright (C) 2012-2013 Bitergia
 #
@@ -20,8 +19,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-#
-# This file is a part of the VizGrimoireJS package
 #
 # Authors:
 #   Alvaro del Castillo San Felix <acs@bitergia.com>
