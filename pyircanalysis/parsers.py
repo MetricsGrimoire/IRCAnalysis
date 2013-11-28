f2775cb5a3896e535937c50e16fdef74e5e53df5
Santiago Dueñas <sduenas@bitergia.com>
2013-11-28 16:38:47 +0100

    [parsers] Support of different formats for HTML based IRC logs

--- pyircanalysis/parsers.py
+++ pyircanalysis/parsers.py
@@ -185,9 +185,61 @@ class HTMLLogParser(LogParser):
 
     TODO: activate Unicode support.
     """
+    def __init__(self, infile, dircproxy=False):
+        LogParser.__init__(self, infile, dircproxy)
+
+        if self.dircproxy:
+            self.NICK_REGEXP = self.DIRCPROXY_NICK_REGEXP
+
+    def _read_html(self):
+        html = self.infile.read()
+        return BeautifulSoup(html)
+
+    def _read_entries(self, html):
+        logtable = html.find('table', attrs={'class': 'irclog'})
+        if not logtable:
+            return []
+        return [entry for entry in logtable.find_all('tr')]
+
+    def _get_handler(self, entries):
+        if not entries:
+            return TextHTMLEntryHandler
+
+        sample = entries[0]
+        if TextHTMLEntryHandler.check_format(sample):
+            return TextHTMLEntryHandler
+        else:
+            return TableHTMLEntryHandler
+
+    def __iter__(self):
+        html = self._read_html()
+        self.entries = self._read_entries(html)
+        self.handler = self._get_handler(self.entries)
+
+        for entry in self.entries:
+            if not entry:
+                continue
+            yield self.handler.handle(entry)
+
+
+class HTMLEntryHandler(object):
+    """Abstract class for handling HTML log entries."""
+
+    @staticmethod
+    def check_format(entry):
+        raise NotImplementedError
+
+    @staticmethod
+    def handle(entry):
+        raise NotImplementedError
+
+
+class TextHTMLEntryHandler(HTMLEntryHandler):
+    """Handler for based text log entries."""
+
     TIME_REGEXP = re.compile(
             r'^\[?(' # Optional [
-            r'(?:\d{4}-\d{2}-\d{2}T|\d{4}/\d{2}/\d{2} |\d{2}-\w{3}-\d{4} |\w{3} \d{2} |\d{2} \w{3} )?' # Optional date
+            r'(?:\d{4}-\d{2}-\d{2}T|\d{4}/\d{2}/\d{2} |\d{2}-\w{3}-\d{4} |\w{3} \d{2} |\d{2} \w{3})?' # Optional date
             r'\d\d:\d\d(:\d\d)?' # Mandatory HH:MM, optional :SS
             r')\]? +') # Optional ], mandatory space
     NICK_REGEXP = re.compile(r'^<(.*?)(!.*)?>\s')
@@ -195,48 +247,84 @@ class HTMLLogParser(LogParser):
     JOIN_REGEXP = re.compile(r'^(?:@)\s.*joined')
     PART_REGEXP = re.compile(r'^(?:@)\s.*(Quit|left)')
     SERVMSG_REGEXP = re.compile(r'^(?:@)\s')
+    NICK_CHANGE_REGEXP = re.compile(r'^@ (.+) is now known as (.+)')
 
-    def __init__(self, infile, dircproxy=False):
-        LogParser.__init__(self, infile, dircproxy)
+    @staticmethod
+    def check_format(entry):
+        if entry.td['class'][0] != 'other':
+            return False
 
-        if self.dircproxy:
-            self.NICK_REGEXP = self.DIRCPROXY_NICK_REGEXP
+        content = entry.td.text
+        m = TextHTMLEntryHandler.TIME_REGEXP.match(content)
+        return m is not None
 
-    def _read_lines(self):
-        html = self.infile.read()
-        bs = BeautifulSoup(html)
-        lines = [line.text
-                for line in bs.find('table', attrs={'class': 'irclog'}).find_all('td')]
-        return lines
+    @staticmethod
+    def handle(entry):
+        content = entry.text.encode('utf-8')
 
-    def __iter__(self):
-        self.lines = self._read_lines()
+        m = TextHTMLEntryHandler.TIME_REGEXP.match(content)
+        if m:
+            time = dateutil.parser.parse(m.group(1))
+            content = content[len(m.group(0)):]
+        else:
+            time = None
 
-        for line in self.lines:
-            line = line.encode('utf-8')
+        m = TextHTMLEntryHandler.NICK_REGEXP.match(content)
+        if m:
+            nick = m.group(1)
+            text = content[len(m.group(0)):]
+            return time, LogParser.COMMENT, (nick, text)
+        elif content.startswith('* ') or content.startswith('*\t'):
+            return time, LogParser.ACTION, content
+        elif TextHTMLEntryHandler.JOIN_REGEXP.match(content):
+            return time, LogParser.JOIN, content
+        elif TextHTMLEntryHandler.PART_REGEXP.match(content):
+            return time, LogParser.PART, content
+        else:
+            m = TextHTMLEntryHandler.NICK_CHANGE_REGEXP.match(content)
+            if m:
+                oldnick = m.group(1)
+                newnick = m.group(2)
+                return time, LogParser.NICKCHANGE, (content, oldnick, newnick)
+            elif TextHTMLEntryHandler.SERVMSG_REGEXP.match(content):
+                return time, LogParser.SERVER, content
+            else:
+                return time, LogParser.OTHER, content
 
-            if not line:
+
+class TableHTMLEntryHandler(HTMLEntryHandler):
+    """Handler for HTML table format log entries."""
+
+    NICK_CHANGE_REGEXP = re.compile(r'^\*\*\*\s+(.+) is now known as (.+)')
+
+    @staticmethod
+    def handle(entry):
+        time = dateutil.parser.parse(entry['id'])
+
+        tag = entry.find('th', attrs={'class': 'nick'})
+        if tag:
+            nick = tag.string
+            content = entry.find('td', attrs={'class': 'text'})
+            content = content.text.encode('utf-8')
+            return time, LogParser.COMMENT, (nick, content)
+
+        tags = entry.find_all('td')
+        for tag in tags:
+            if not tag.has_attr('class'):
                 continue
 
-            m = self.TIME_REGEXP.match(line)
-            if m:
-                time = dateutil.parser.parse(m.group(1))
-                line = line[len(m.group(0)):]
-            else:
-                time = None
+            content = tag.text.encode('utf-8')
 
-            m = self.NICK_REGEXP.match(line)
-            if m:
-                nick = m.group(1)
-                text = line[len(m.group(0)):]
-                yield time, self.COMMENT, (nick, text)
-            elif line.startswith('* ') or line.startswith('*\t'):
-                yield time, self.ACTION, line
-            elif self.JOIN_REGEXP.match(line):
-                yield time, self.JOIN, line
-            elif self.PART_REGEXP.match(line):
-                yield time, self.PART, line
-            elif self.SERVMSG_REGEXP.match(line):
-                yield time, self.SERVER, line
+            if tag['class'][0] == 'join':
+                return time, LogParser.JOIN, content
+            elif tag['class'][0] == 'part':
+                return time, LogParser.PART, content
+            elif tag['class'][0] == 'servermsg':
+                return time, LogParser.SERVER, content
+            elif tag['class'][0] == 'nickchange':
+                m = TableHTMLEntryHandler.NICK_CHANGE_REGEXP.match(content)
+                oldnick = m.group(1)
+                newnick = m.group(2)
+                return time, LogParser.NICKCHANGE, (content, oldnick, newnick)
             else:
-                yield time, self.OTHER, line
+                return time, LogParser.OTHER, content
